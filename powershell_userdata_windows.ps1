ab39187cffb7d54341999ca1c0677e3beef5f572
Mr Kumar <kumarawsbit@gmail.com>
2019-12-30 17:18:31 +0530

    Update powershell_userdata_windows.ps1

--- powershell_userdata_windows.ps1
+++ powershell_userdata_windows.ps1
@@ -17,5 +17,9 @@ $output = "c:\kumar\python-3.8.1-amd64.exe"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri $pythonurl -OutFile $output
 New-Item -Path C:\kumar -Name "kumar.txt" -ItemType "file" -Value "Welcome to AWS world."
+[string]$installcmd = ""
+$installcmd += "c:\kumar\python-3.8.1-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0"
+New-Item -Path C:\kumar -Name "execute.bat" -ItemType "file" -Value $installcmd
+Start-Process "cmd.exe"  "/c C:\kumar\execute.bat"
 Write-Host("Program ended")
 </powershell>
